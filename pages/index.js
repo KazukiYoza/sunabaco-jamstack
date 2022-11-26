b947cec1540f391adbcff5472253a1296883fc2c
yoza kazuki <yzkzk0323@gmail.com>
2022-11-26 15:17:12 +0900

    1st commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
+import Link from "next/link";
 import { Container } from "../src/components/Container";
+import { client } from "../src/lib/client";
 
-export default function Home() {
+export default function Home({ books }) {
+	console.log(books);
 	return (
 		<main>
-			<Container>Hello World!!</Container>
+			<Container>
+				{books.map((book) => {
+					return (
+						<Link href={`book/${book.id}`} key={book.id}>
+							<p style={{fontWeight: 'bold' , color: '#555555'} }>
+								【{book.title}】
+							</p>
+						</Link>
+					);
+				})}
+			</Container>
 		</main>
 	);
 }
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+	const data = await client.get({
+		endpoint: "books",
+		queries: { limit: 100 },
+	});
+
+	return {
+		props: {
+			books: data.contents,
+		},
+	};
+};
